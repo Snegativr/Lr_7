da9ad507ca831e2f9f281298ac27c29b9b422a02
Roman <eveningnightforest@gmail.com>
2021-10-23 14:51:16 +0300

    creating firs file

---
+++ C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+int main()
+{
+	float  y, min, max, step;
+	cout << "min=";
+	cin >> min;
+	cout << "max=";
+	cin >> max;
+	cout << "step=";
+	cin >> step;
+	do {
+		
+				y = pow(sin(min), 2) + 2 * cos(min) + 3;
+		cout <<setw(10)<<"y=" << y << setw(10) <<"x="<< min << "\n";
+		min += step;
+
+	} while (min <= max);
+}
